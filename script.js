6f170a9f51156da6b4881e924face68636fc0339
pilu9147 <119696172+pilu9147@users.noreply.github.com>
2023-06-03 12:31:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
-window.promises = [];
 
-// Do not change the code above this
-// add your promises to the array `promises`
+
+const id = document.getElementById("output")
+
+
+let prom1 = new Promise((resolve,reject) =>{
+  setTimeout(() =>{
+    resolve("data1");
+  },3000)
+});
+
+let prom2 = new Promise((resolve,reject) =>{
+  setTimeout(() =>{
+    resolve("data2")
+  },2000)
+});
+let prom3 = new Promise((resolve,reject) =>{
+  setTimeout(() =>{
+    resolve("data3")
+  },1000)
+});
+let prom4 = new Promise((resolve,reject) =>{
+  setTimeout(() =>{
+    resolve("data4")
+  },4000)
+});
+let prom5 = new Promise((resolve,reject) =>{
+  setTimeout(() =>{
+    resolve("data5")
+  },5000)
+});
+window.promises = [prom1, prom2, prom3, prom4, prom5];
+let x = Promise.any([prom1, prom2, prom3, prom4, prom5])
+
+x.then((data) =>{
+
+  // console.log(data);
+  id.innerText = data;
+
+});
+
+x.catch((error) =>{
+  console.log("error :", error)
+});
